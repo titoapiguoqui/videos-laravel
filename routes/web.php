f093d70811b2afb016836011b8864c9f6b5f3454
Héctor Martínez <h.martinez@gobalo.es>
2019-03-20 17:00:59 +0100

    first steps

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola-mundo', function () {
+    return '<h1>¡Hola Mundo!</h1> Soy tu padre.';
+});
+
+Route::get('contacto/{nombre?}/{edad?}', function ($nombre = "Pepito", $edad = 30) {
+    /*return view('contacto', array(
+        "nombre" => $nombre,
+        "edad" => $edad
+    ));*/
+    return view('contacto')->with('nombre', $nombre)
+                           ->with('edad', $edad);
+})->where(([
+    'nombre' => '[A-Za-z]+',
+    'edad' => '[0-9]+'
+]));
